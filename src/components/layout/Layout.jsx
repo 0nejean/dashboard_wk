98d6e9fd78ea462fc67f657ec430d7dbe144857c
0nejean <onejean114@gmail.com>
2023-10-19 14:36:58 +0900

    React d-8

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import Header from './header/Header';
-import styled from 'styled-components';
-import Title from '../common/Tilte';
-import History from '../common/History';
-import { Box } from '@chakra-ui/react';
+import React from "react";
+import Header from "./header/Header";
+import styled from "styled-components";
+import Title from "../common/Tilte";
+import History from "../common/History";
+import { Box } from "@chakra-ui/react";
 
 const Layout = ({ pagename, children }) => {
-    // const { pagename, children } = props;
-    return (
-        <Wrap>
-            <Header />
-            <main id="main">
-                <Box pl="10px">
-                    <History pagename={pagename} />
-                    <Title pagename={pagename} />
-                </Box>
-                {children}
-            </main>
-        </Wrap>
-    );
+  // const { pagename, children } = props;
+  return (
+    <Wrap>
+      <Header />
+      <main id="main">
+        <Box p="0 0 35px 10px">
+          <History pagename={pagename} />
+          <Title pagename={pagename} />
+        </Box>
+        {children}
+      </main>
+    </Wrap>
+  );
 };
 
 const Wrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    padding-left: 290px;
-    background: var(--secondary-grey-300, #f4f7fe);
-    #main {
-        flex: 1;
-        padding: 50px 20px;
-    }
+  display: flex;
+  flex-direction: column;
+  padding-left: 290px;
+  background: var(--secondary-grey-300, #f4f7fe);
+  #main {
+    padding: 50px 20px;
+  }
 `;
 
 export default Layout;
--- src/pages/Tables.jsx
+++ src/pages/Tables.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import { Grid, GridItem } from '@chakra-ui/react';
-import Layout from '../components/layout/Layout';
+import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Layout from "../components/layout/Layout";
+import Development from "../components/tables/Development";
 
 const Tables = () => {
-    return (
-        <Layout pagename={'Tables'}>
-            <Grid flexGrow={'1'} templateRows="repeat(2, 1fr)" templateColumns="repeat(2, 1fr)" gap={4}>
-                <GridItem bg="yellow" />
-                <GridItem bg="papayawhip" />
-                <GridItem bg="blue" />
-                <GridItem bg="tomato" />
-            </Grid>
-        </Layout>
-    );
+  return (
+    <Layout pagename={"Tables"}>
+      <Grid
+        h={"calc(100vh - 50px * 2 - 97px)"}
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(2, 1fr)"
+        gap={4}
+      >
+        <GridItem colSpan={1} bg="yellow">
+          <Development />
+        </GridItem>
+        <GridItem bg="papayawhip" />
+        <GridItem bg="blue" />
+        <GridItem bg="tomato" />
+      </Grid>
+    </Layout>
+  );
 };
 
 export default Tables;
